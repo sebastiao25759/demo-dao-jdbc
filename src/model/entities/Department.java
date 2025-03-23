02da62ea8b7a72a6466f93b158eb5e86a5e2a5c6
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-03-22 22:40:35 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+    public Department() {
+
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getInt() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
