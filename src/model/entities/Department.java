c014e93f6515e1d1a3fa14dc92e94e9e86b77f6b
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-03-30 15:37:44 -0300

    DepartmentDao implementation and test

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -12,7 +12,7 @@ public class Department implements Serializable {
 
     }
 
-    public Department(int id, String name) {
+    public Department(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
